e0992633fc6427e5bf08c9791e7d918670b3fb82
veedz <m.al.hafizd@gmail.com>
2026-01-12 00:51:36 +0700

    Update reboot.c

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -314,8 +314,13 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	struct pid_namespace *pid_ns = task_active_pid_ns(current);
 	char buffer[256];
 	int ret = 0;
-#ifdef CONFIG_KSU
-	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+#ifdef CONFIG_KSU_SUSFS
+    ret = ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+    if (ret) {
+        goto orig_flow;
+    }
+    return ret;
+orig_flow:
 #endif
 	/* We only trust the superuser with rebooting the system. */
 	if (!ns_capable(pid_ns->user_ns, CAP_SYS_BOOT))
