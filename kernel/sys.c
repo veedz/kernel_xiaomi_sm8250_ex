78fa004235b0b27125c462d633da548f6b2310eb
veedz <m.al.hafizd@gmail.com>
2026-01-29 19:52:57 +0700

    Update sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1258,7 +1258,13 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
-	memcpy(&tmp, utsname(), sizeof(tmp));
+	memcpy(&tmp, utsname(), sizeof(tmp));	
+	if (current_uid().val == 0 &&
+		(!strncmp(current->comm, "netd", 4))) {
+		strcpy(tmp.release, "5.10.248");
+		pr_info("fake uname: %s/%d release=%s\n",
+			 current->comm, current->pid, tmp.release);
+}
 	#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	susfs_spoof_uname(&tmp);
 #endif
